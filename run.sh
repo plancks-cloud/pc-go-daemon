7f7c276b80e23cbb62c9cdf2a18717c4e69a9218
Kevin Stroud <g33k3n01d@gmail.com>
2018-03-16 16:11:37 +0200

    create run file

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker stop pc-go-daemon
+docker rm pc-go-daemon
+docker rmi pc-go-daemon:latest
+
+export GOOS=linux
+
+go build
+
+docker build -t pc-go-daemon:latest .
+docker run --name pc-go-daemon -v /var/run/docker.sock:/var/run/docker.sock --publish 8080:8080 pc-go-daemon:latest
