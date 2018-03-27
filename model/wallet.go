3c03b9fcaea47e77c6de439101310fa6d12a9651
Justin Tamblyn <just1689@gmail.com>
2018-03-27 20:46:17 +0200

    Wallet

--- model/wallet.go
+++ model/wallet.go
@@ -1,22 +1,5 @@
 package model
 
-<<<<<<< HEAD
-import "git.amabanana.com/plancks-cloud/pc-go-daemon/mongo"
-
-//Wallet represents a unique node
-type Wallet struct {
-	ID         string `db:"_id" json:"_id"`
-	PrivateKey string
-	PublicKey  string
-}
-
-//GetSignature will get sign an object
-func (wallet *Wallet) GetSignature(obj interface{}) string {
-	return wallet.ID
-}
-
-//Push saves a wallet to MongoDB
-=======
 import (
 	"git.amabanana.com/plancks-cloud/pc-go-daemon/mongo"
 )
@@ -29,13 +12,10 @@ type Wallet struct {
 }
 
 //Push persists the wallet to the database
->>>>>>> origin/master
 func (wallet Wallet) Push() error {
 	err := mongo.Push(wallet)
 	return err
 }
-<<<<<<< HEAD
-=======
 
 //Signature returns the wallet ID
 func (wallet Wallet) Signature() string {
@@ -46,4 +26,3 @@ func (wallet Wallet) Signature() string {
 func (wallet Wallet) Upsert() error {
 	return mongo.UpsertWithID(wallet.ID, wallet)
 }
->>>>>>> origin/master
