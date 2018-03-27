085cea2df54a528e6411e08f832e550136b7f209
Justin Tamblyn <just1689@gmail.com>
2018-03-27 20:59:59 +0200

    Mutators on win

--- model/win.go
+++ model/win.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"git.amabanana.com/plancks-cloud/pc-go-daemon/mongo"
+)
+
 //Win object represents bid document in DB
 type Win struct {
 	ID            string `json:"_id,omitempty" bson:"_id,omitempty"`
@@ -7,4 +11,15 @@ type Win struct {
 	WinnerAccount string
 	timestamp     int64
 	signature     string
+}
+
+//Push persists the win to the database
+func (item Win) Push() error {
+	err := mongo.Push(item)
+	return err
+}
+
+//Upsert updates a document if it exists, otherwise inserts
+func (item Win) Upsert() error {
+	return mongo.UpsertWithID(item.ID, item)
 }
