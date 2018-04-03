b7167f55ec3a89f8bc47203328fab26ba85c0522
Justin Tamblyn <just1689@gmail.com>
2018-04-03 14:29:45 +0200

    Refactor for response utils

--- api/wallet.go
+++ api/wallet.go
@@ -8,23 +8,24 @@ import (
 	"git.amabanana.com/plancks-cloud/pc-go-daemon/controller"
 	"git.amabanana.com/plancks-cloud/pc-go-daemon/model"
 	log "github.com/sirupsen/logrus"
+	"git.amabanana.com/plancks-cloud/pc-go-daemon/util"
 )
 
 //CreateWallet creates a new wallet
 func CreateWallet(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	decoder := json.NewDecoder(r.Body)
 	var wallet model.Wallet
 	err := decoder.Decode(&wallet)
 	if err != nil {
 		log.Errorln(fmt.Sprintf("There was a problem decoding the post message: %s", err))
-		json.NewEncoder(w).Encode(model.OkMessage(false, err.Error()))
+		util.RespondWithJson(w, model.OkMessage(false, err.Error()))
+		return
 	}
-	json.NewEncoder(w).Encode(controller.CreateWallet(&wallet))
+	util.RespondWithJson(w, controller.CreateWallet(&wallet))
+
 }
 
 //SetCurrentWallet sets the currently used wallet
 func SetCurrentWallet(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(controller.SetCurrentWallet())
+	util.RespondWithJson(w, controller.SetCurrentWallet())
 }
