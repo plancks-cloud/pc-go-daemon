3c452eedef1ae39dab83477d1f020a8f0a232db0
Justin Tamblyn <just1689@gmail.com>
2018-04-03 15:58:56 +0200

    Heresy

--- util/response.go
+++ util/response.go
@@ -3,6 +3,7 @@ package util
 import (
 	"net/http"
 	"encoding/json"
+	"fmt"
 	"git.amabanana.com/plancks-cloud/pc-go-daemon/model"
 )
 
@@ -14,11 +15,34 @@ func RespondWithJsonObject(w http.ResponseWriter, v interface{}) {
 }
 
 func RespondWithJsonOk(w http.ResponseWriter, v interface{}) {
-	RespondWithJsonObject(w, model.Ok(true))
+	RespondWithJsonObject(w, Ok(true))
 
 }
 
 func RespondWithJsonError(w http.ResponseWriter, e error) {
-	RespondWithJsonObject(w, model.OkMessage(false, e.Error()))
+	RespondWithJsonObject(w, OkMessage(false, e.Error()))
 
+}
+
+//MessageOK represents a successful http call
+type MessageOK struct {
+	Ok      bool   `json:"ok"`
+	Message string `json:"message,omitempty"`
+}
+
+func (message *MessageOK) String() string {
+	if message.Ok == true {
+		return "true"
+	}
+	return fmt.Sprintf("false, %s", message.Message)
+}
+
+//Ok returns a message object with the set state
+func Ok(state bool) MessageOK {
+	return MessageOK{Ok: state}
+}
+
+//OkMessage returns a message object with the set state
+func OkMessage(state bool, message string) MessageOK {
+	return MessageOK{Ok: state, Message: message}
 }
