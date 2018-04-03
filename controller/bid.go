b7336fa760c371b5500a28aa8703b71f4eed6082
Justin Tamblyn <just1689@gmail.com>
2018-04-03 14:18:27 +0200

    Clean up bids

--- controller/bid.go
+++ controller/bid.go
@@ -1,25 +1,13 @@
 package controller
 
 import (
-	"fmt"
 	"math/rand"
 
 	"git.amabanana.com/plancks-cloud/pc-go-daemon/model"
 	"git.amabanana.com/plancks-cloud/pc-go-daemon/mongo"
 	"git.amabanana.com/plancks-cloud/pc-go-daemon/util"
 	"github.com/globalsign/mgo/bson"
-	log "github.com/sirupsen/logrus"
 )
-
-//CreateBid creates a new contract
-func CreateBid(bid *model.Bid) model.MessageOK {
-	err := bid.Push()
-	if err != nil {
-		log.Errorln(fmt.Sprintf("Error saving bid: %s", err))
-		return model.OkMessage(false, err.Error())
-	}
-	return model.Ok(true)
-}
 
 //GetBid returns all contracts stored in the datastore
 func GetBid() []model.Bid {
@@ -33,22 +21,6 @@ func GetBidsByContractID(contractID string) []model.Bid {
 	var bids []model.Bid
 	mongo.GetCollection(model.Bid{}).Find(bson.M{"contractId": contractID}).All(&bids)
 	return bids
-}
-
-//GetOneBid returns a single contract
-func GetOneBid(id string) (model.Bid, error) {
-	var bid model.Bid
-	err := mongo.GetCollection(&bid).Find(bson.M{"_id": id}).One(&bid)
-	if err != nil {
-		log.Errorln(fmt.Sprintf("Error getting bid: %s", err))
-	}
-	return bid, err
-}
-
-//UpdateBid upserts the given bid
-func UpdateBid(bid *model.Bid) error {
-	err := bid.Upsert()
-	return err
 }
 
 //CreateBidFromContract inserts a new bid for a contract
