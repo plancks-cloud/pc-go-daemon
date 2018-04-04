0731373300db5abff6d78fc0edfb702dcc8355a1
Captain Justin <just1689@gmail.com>
2018-04-04 21:05:49 +0200

    Justin dev runnable

---
+++ justin.bat
@@ -0,0 +1,7 @@
+set export GOOS=linux
+docker stop daemony
+docker rm daemony
+docker rmi daemony:latest
+go build
+docker build -t daemony:latest .
+docker stack deploy pc --compose-file docker-compose-justin.yml
