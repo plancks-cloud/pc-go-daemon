95444ae53327756b02e68f9cb58c92394ecda14c
Justin Tamblyn <just1689@gmail.com>
2018-04-04 16:37:51 +0200

    Updated run

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-docker stop daemony
-docker rm daemony
 docker rmi daemony:latest
-
 export GOOS=linux
-
 go build
-
 docker build -t daemony:latest .
 
--- run.sh
+++ run.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# docker stop pc-go-daemon
-# docker rm pc-go-daemon
-# docker rmi pc-go-daemon:latest
-
-# export GOOS=linux
-
-# go build
-
-# docker build -t pc-go-daemon:latest .
-# docker run --name pc-go-daemon -v /var/run/docker.sock:/var/run/docker.sock --publish 8080:8080 pc-go-daemon:latest
-
+docker stack rm pc
 ./build.sh
 docker stack deploy pc --compose-file docker-compose.yml
