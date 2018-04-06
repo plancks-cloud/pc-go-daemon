7e15e595fe8e0141807cce50d070435aa31152b2
Kevin Stroud <g33k3n01d@gmail.com>
2018-04-06 15:45:58 +0200

    deploy scripts

--- kevin.sh
+++ kevin.sh
@@ -11,3 +11,12 @@ sleep 1s
 
 ./build.sh
 docker stack deploy pc --compose-file docker-compose-kevin-mac.yml
+
+echo "Getting daemony container ID"
+sleep 2s
+
+DAEMONID=`docker ps | grep daemony | grep -o "^[0-9a-zA-Z]" |  tr -d '\n' | awk '{$1=$1};1'`
+
+echo "Running daemony: ${DAEMONID}"
+echo "Connecting to logs:"
+docker logs -f ${DAEMONID}
--- run.sh
+++ run.sh
@@ -3,3 +3,10 @@
 docker stack rm pc
 ./build.sh
 docker stack deploy pc --compose-file docker-compose.yml
+
+echo "Getting daemony container ID"
+sleep 2s
+
+DAEMONID=`docker ps | grep daemony | grep -o "^[0-9a-zA-Z]" |  tr -d '\n' | awk '{$1=$1};1'`
+
+echo "Running daemony: ${DAEMONID}"
