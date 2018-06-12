2e0f52c32abcf177d16ecbd2dd8c83f1487fb42c
Captain Justin <just1689@gmail.com>
2018-06-12 21:48:56 +0200

    Test for empty

--- util/enc_test.go
+++ util/enc_test.go
@@ -8,6 +8,8 @@ import (
 func TestGetPublicPrivateKey(t *testing.T) {
 	private, public := GeneratePrivatePublicKeys()
 	assert.NotNil(t, private, "The private key should not be nil")
+	assert.NotEmpty(t, private, "The private key should not be empty")
 	assert.NotNil(t, public, "The public key should not be nil")
+	assert.NotEmpty(t, public, "The public key should not be empty")
 
 }
