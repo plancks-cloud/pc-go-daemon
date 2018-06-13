427f40e19b427110a0de0a67a716bd84902cb67c
Justin Tamblyn <just1689@gmail.com>
2018-06-13 07:56:45 +0200

    Clean up - do not expose methods unintentionally

--- util/enc.go
+++ util/enc.go
@@ -9,54 +9,52 @@ import (
 )
 
 func GeneratePrivatePublicKeys() (privateKey string, publicKey string) {
-	//fmt.Println("Starting the application...")
-	wif, _ := Networks["btc"].CreatePrivateKey()
-	address, _ := Networks["btc"].GetAddress(wif)
-	//fmt.Printf("%s - %s", wif.String(), address.EncodeAddress())
+	wif, _ := networks["btc"].createPrivateKey()
+	address, _ := networks["btc"].getAddress(wif)
 	return wif.String(), address.EncodeAddress()
 
 }
 
-type Network struct {
+type network struct {
 	name        string
 	symbol      string
 	xpubkey     byte
 	xprivatekey byte
 }
 
-var Networks = map[string]Network{
+var networks = map[string]network{
 	"rdd": {name: "reddcoin", symbol: "rdd", xpubkey: 0x3d, xprivatekey: 0xbd},
 	"dgb": {name: "digibyte", symbol: "dgb", xpubkey: 0x1e, xprivatekey: 0x80},
 	"btc": {name: "bitcoin", symbol: "btc", xpubkey: 0x00, xprivatekey: 0x80},
 	"ltc": {name: "litecoin", symbol: "ltc", xpubkey: 0x30, xprivatekey: 0xb0},
 }
 
-func (network Network) GetNetworkParams() *chaincfg.Params {
+func (network network) getNetworkParams() *chaincfg.Params {
 	networkParams := &chaincfg.MainNetParams
 	networkParams.PubKeyHashAddrID = network.xpubkey
 	networkParams.PrivateKeyID = network.xprivatekey
 	return networkParams
 }
 
-func (network Network) CreatePrivateKey() (*btcutil.WIF, error) {
+func (network network) createPrivateKey() (*btcutil.WIF, error) {
 	secret, err := btcec.NewPrivateKey(btcec.S256())
 	if err != nil {
 		return nil, err
 	}
-	return btcutil.NewWIF(secret, network.GetNetworkParams(), true)
+	return btcutil.NewWIF(secret, network.getNetworkParams(), true)
 }
 
-func (network Network) ImportWIF(wifStr string) (*btcutil.WIF, error) {
+func (network network) importWIF(wifStr string) (*btcutil.WIF, error) {
 	wif, err := btcutil.DecodeWIF(wifStr)
 	if err != nil {
 		return nil, err
 	}
-	if !wif.IsForNet(network.GetNetworkParams()) {
+	if !wif.IsForNet(network.getNetworkParams()) {
 		return nil, errors.New("The WIF string is not valid for the `" + network.name + "` network")
 	}
 	return wif, nil
 }
 
-func (network Network) GetAddress(wif *btcutil.WIF) (*btcutil.AddressPubKey, error) {
-	return btcutil.NewAddressPubKey(wif.PrivKey.PubKey().SerializeCompressed(), network.GetNetworkParams())
+func (network network) getAddress(wif *btcutil.WIF) (*btcutil.AddressPubKey, error) {
+	return btcutil.NewAddressPubKey(wif.PrivKey.PubKey().SerializeCompressed(), network.getNetworkParams())
 }
